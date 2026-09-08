570e6c7be3215669255486e922b71c2c7858722f
MeY0u <154277071+MeY0u@users.noreply.github.com>
2026-09-08 04:15:18 +0300

    Fix notification builder compatibility

--- app/src/main/java/com/sinoise/app/ReminderReceiver.java
+++ app/src/main/java/com/sinoise/app/ReminderReceiver.java
@@ -52,7 +52,6 @@ public class ReminderReceiver extends BroadcastReceiver {
                 .setContentIntent(contentIntent)
                 .setAutoCancel(true)
                 .setOnlyAlertOnce(true)
-                .setSilent(true)
                 .build();
 
         NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
