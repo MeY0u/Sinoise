f5647ab315df0646482333f82d964265c43515cf
MeY0u <154277071+MeY0u@users.noreply.github.com>
2026-09-08 04:11:13 +0300

    Add boot receiver

---
+++ app/src/main/java/com/sinoise/app/BootReceiver.java
@@ -0,0 +1,12 @@
+package com.sinoise.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ReminderScheduler.restore(context);
+    }
+}
